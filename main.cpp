62ad99c183e5d67d3d75bf39d60e0e8adc16a00d
João Vitor Bazzanella <jvbazzanella@hotmail.com>
2019-03-23 17:22:33 -0300

    0.1
    teste

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+#include<carro.h>
+#include<aviao.h>
+#include<barco.h>
+
+int main(){
+  
+}
---
+++ veiculo.h
@@ -0,0 +1,24 @@
+#ifndef VEICULO_H
+#define VEICULO_H
+
+
+  class veiculo{
+
+  private:
+
+    string marca;
+    int cavalos;
+    int capacidade
+
+
+  public:
+
+    string getMarca() const;
+    int getCavalos() const;
+    int getCapacidade() const;
+
+    void setMarca() const
+    void setCavalos() const;
+    void setCapacidade() const;
+  }
+#endif
